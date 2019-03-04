e7a6f4d0ab0eb3caba68ced19ac775afda920a49
ketangote <ketan.gote@metamagic.in>
2019-03-04 12:08:03 +0530

    Update

--- src/main/java/com/metamagic/ms/WebController.java
+++ src/main/java/com/metamagic/ms/WebController.java
@@ -1,16 +1,10 @@
 package com.metamagic.ms;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @CrossOrigin()
